9136710cc847bc5eb8a49dd0fd8b1be29bfd1e05
AlfaJr11 <juniorjalo11@gmail.com>
2026-04-14 03:02:12 +0200

    Actualizada la cabecera principal de la aplicación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+
         GestorTareas gestor = new GestorTareas();
-        System.out.println("=== GESTOR DE TAREAS ===");
+        System.out.println("=== ORGANIZADOR PERSONAL DE TAREAS ===");
         gestor.listarTareas();
     }
 }
